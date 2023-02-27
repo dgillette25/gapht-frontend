022f196030ebd04d8f14f9c456224eb9c970fdca
Dano Gillette <dgillette25@gmail.com>
2023-02-26 23:52:22 -0700

    Subheader and removing modules I don't need

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { AppComponent } from './app.component';
 import { ConversationComponent } from './conversation/conversation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
@@ -25,10 +23,8 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     FormsModule,
     ReactiveFormsModule,
     MatInputModule,
-    MatGridListModule,
     MatCardModule,
     MatProgressSpinnerModule,
-    MatDividerModule,
     MatButtonModule
   ],
   providers: [],
