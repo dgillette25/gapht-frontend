34e5b8a148fa4d57e7678bd97fb2b4b9c4368fce
Dano Gillette <dgillette25@gmail.com>
2023-02-27 00:04:21 -0700

    new url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://gapht.us-west-2.elasticbeanstalk.com/'
+  apiUrl: 'http://data.gapht.us'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://gapht.us-west-2.elasticbeanstalk.com'
+  apiUrl: 'http://data.gapht.us'
 };
